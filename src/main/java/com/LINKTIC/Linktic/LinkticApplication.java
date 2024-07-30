c9c8390da6d2db80d7313aee342237830514e4a7
Nicolas Ruiz <nicolas.ruiz@bitechconsulting.co>
2024-07-30 11:35:01 -0500

    First commit

---
+++ src/main/java/com/LINKTIC/Linktic/LinkticApplication.java
@@ -0,0 +1,13 @@
+package com.LINKTIC.Linktic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LINKTIC/Linktic/LinkticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LINKTIC.Linktic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
